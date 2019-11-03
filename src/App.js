3eab8db5197b79a01ed7b71015bba803492d9602
haipayazoo <haipayazoo@gmail.com>
2019-11-02 18:29:05 -0700

    refactoring + adding facerecognition image bounds

--- src/App.js
+++ src/App.js
@@ -29,25 +29,26 @@ class App extends Component {
     super();
     this.state = {
       input: '',
+      imageUrl: '',
+      box: {},
     }
   }
 
+  calculateFaceLocation = (data) => {
+
+  }
+
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({input: event.target.value});
   }
 
   onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
     app.models.predict(
-      "a403429f2ddf4b49b307e318f00e528b", 
-      "https://samples.clarifai.com/face-det.jpg")
-    .then(
-      function(response) {
-        console.log(response);
-      },
-      function(err) {
-        // there was an error
-      }
-    );
+      Clarifai.FACE_DETECT_MODEL, 
+      this.state.input)
+    .then(response => this.calculateFaceLocation(response))
+    .catch(err => console.log(err));
   }
 
   render() {
@@ -63,7 +64,7 @@ class App extends Component {
           onInputChange={this.onInputChange} 
           onButtonSubmit={this.onButtonSubmit}
         />
-        <FaceRecognition />
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
