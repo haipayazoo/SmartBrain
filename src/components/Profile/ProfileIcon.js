2237b01fc21915b2c9aabd292252cf8b29c385f5
haipayazoo <haipayazoo@gmail.com>
2020-04-02 19:41:49 -0700

    Added dropdown menu for profileicon

--- src/components/Profile/ProfileIcon.js
+++ src/components/Profile/ProfileIcon.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import {
+	Dropdown,
+	DropdownToggle,
+	DropdownMenu,
+	DropdownItem
+} from 'reactstrap';
 
 class ProfileIcon extends React.Component {
 	constructor(props) {
@@ -8,14 +14,37 @@ class ProfileIcon extends React.Component {
 		};
 	}
 
+	toggle = () => {
+		this.setState(prevState => ({
+			dropdownOpen: !prevState.dropdownOpen
+		}));
+	};
+
 	render() {
 		return (
-			<div>
-				<img
-					src="http://tachyons.io/img/logo.jpg"
-					class="br-100 ba h3 w3 dib"
-					alt="avatar"
-				/>
+			<div className="pa4 tc">
+				<Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+					<DropdownToggle
+						tag="span"
+						data-toggle="dropdown"
+						aria-expanded={this.state.dropdownOpen}
+					>
+						<img
+							src="http://tachyons.io/img/logo.jpg"
+							className="br-100 ba h3 w3 dib"
+							alt="avatar"
+						/>
+					</DropdownToggle>
+					<DropdownMenu
+						className="b--transparent shadow-5"
+						style={{
+							backgroundColor: 'rgba(255,255,255, 0.5)'
+						}}
+					>
+						<DropdownItem>View Profile</DropdownItem>
+						<DropdownItem>Sign Out</DropdownItem>
+					</DropdownMenu>
+				</Dropdown>
 			</div>
 		);
 	}
