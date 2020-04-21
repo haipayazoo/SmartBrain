2201d2b54d43762760cba1b01623bdf6fb2cc77e
haipayazoo <haipayazoo@gmail.com>
2020-04-21 11:37:16 -0700

    Added ranking functionality

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,16 +1,47 @@
 import React from 'react';
 
-const Rank = ({ name, entries }) => {
-    return (
-        <div>
-            <div className='white f3'>
-                {`${name}, your current entry count is...`}
-            </div>
-            <div className='white f1'>
-                {entries}
-            </div>
-        </div>
-    );
+class Rank extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			style: '',
+		};
+	}
+
+	componentDidMount() {
+		this.generateStyle(this.props.entries);
+	}
+
+	componentDidUpdate(prevProps, prevState) {
+		if (
+			prevProps.entries === this.props.entries &&
+			prevProps.name === this.props.name
+		)
+			return null;
+
+		this.generateStyle(this.props.entries);
+	}
+
+	generateStyle = (entries) => {
+		fetch(
+			`https://jgv0of8fgb.execute-api.us-east-1.amazonaws.com/dev/rank?rank=${entries}`
+		)
+			.then((response) => response.json())
+			.then((data) => this.setState({ style: data.input }))
+			.catch(console.log);
+	};
+
+	render() {
+		return (
+			<div>
+				<div className="white f3">
+					{`${this.props.name}, your current entry count is...`}
+				</div>
+				<div className="white f1">{this.props.entries}</div>
+				<div className="white f3">{`Rank Badge: ${this.state.style}`}</div>
+			</div>
+		);
+	}
 }
 
 export default Rank;
